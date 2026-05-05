e00107a29410e694681985a35801ef3f9ce20add
ArCyln <119622882+ArCyln@users.noreply.github.com>
2026-05-05 15:52:17 +0300

    Add files via upload

---
+++ 243154004 Arda Ceylan 05.05.2026/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txturun = new System.Windows.Forms.TextBox();
+            this.txtkategori = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.lbldurum = new System.Windows.Forms.Label();
+            this.txtfiyat = new System.Windows.Forms.MaskedTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.formToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtabout = new System.Windows.Forms.TextBox();
+            this.çıkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txturun
+            // 
+            this.txturun.Location = new System.Drawing.Point(87, 249);
+            this.txturun.Name = "txturun";
+            this.txturun.Size = new System.Drawing.Size(200, 20);
+            this.txturun.TabIndex = 0;
+            this.txturun.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txturun_KeyPress);
+            // 
+            // txtkategori
+            // 
+            this.txtkategori.Location = new System.Drawing.Point(87, 284);
+            this.txtkategori.Name = "txtkategori";
+            this.txtkategori.Size = new System.Drawing.Size(200, 20);
+            this.txtkategori.TabIndex = 1;
+            this.txtkategori.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtkategori_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(750, 191);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ürün:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Kategori:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Fiyat:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(23, 376);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnEkle.TabIndex = 15;
+            this.btnEkle.Text = "Kayıt Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(131, 375);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(100, 23);
+            this.btnGuncelle.TabIndex = 16;
+            this.btnGuncelle.Text = "Kayıt Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(237, 375);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(100, 23);
+            this.btnSil.TabIndex = 17;
+            this.btnSil.Text = "Kayıt Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(343, 375);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(100, 23);
+            this.btnTemizle.TabIndex = 18;
+            this.btnTemizle.Text = "Formu Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // lbldurum
+            // 
+            this.lbldurum.AutoSize = true;
+            this.lbldurum.Location = new System.Drawing.Point(472, 381);
+            this.lbldurum.Name = "lbldurum";
+            this.lbldurum.Size = new System.Drawing.Size(38, 13);
+            this.lbldurum.TabIndex = 19;
+            this.lbldurum.Text = "Durum";
+            // 
+            // txtfiyat
+            // 
+            this.txtfiyat.Location = new System.Drawing.Point(87, 321);
+            this.txtfiyat.Name = "txtfiyat";
+            this.txtfiyat.Size = new System.Drawing.Size(200, 20);
+            this.txtfiyat.TabIndex = 20;
+            this.txtfiyat.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtfiyat_KeyPress);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.çıkToolStripMenuItem,
+            this.formToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // formToolStripMenuItem
+            // 
+            this.formToolStripMenuItem.Name = "formToolStripMenuItem";
+            this.formToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.formToolStripMenuItem.Text = "Hakkımızda";
+            this.formToolStripMenuItem.Click += new System.EventHandler(this.formToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtabout
+            // 
+            this.txtabout.Location = new System.Drawing.Point(23, 32);
+            this.txtabout.Multiline = true;
+            this.txtabout.Name = "txtabout";
+            this.txtabout.Size = new System.Drawing.Size(750, 174);
+            this.txtabout.TabIndex = 22;
+            this.txtabout.Visible = false;
+            // 
+            // çıkToolStripMenuItem
+            // 
+            this.çıkToolStripMenuItem.Name = "çıkToolStripMenuItem";
+            this.çıkToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.çıkToolStripMenuItem.Text = "Form";
+            this.çıkToolStripMenuItem.Click += new System.EventHandler(this.çıkToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(629, 239);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtabout);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.txtfiyat);
+            this.Controls.Add(this.lbldurum);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtkategori);
+            this.Controls.Add(this.txturun);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "İlaç Kayıt Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txturun;
+        private System.Windows.Forms.TextBox txtkategori;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Label lbldurum;
+        private System.Windows.Forms.MaskedTextBox txtfiyat;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem formToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox txtabout;
+        private System.Windows.Forms.ToolStripMenuItem çıkToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 243154004 Arda Ceylan 05.05.2026/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 
+{ 
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbldurum.Visible = false;
+        }
+
+        private void SaveToFile()
+        {
+            string filePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                "243154004 Arda Ceylan 05.05.2026",
+                "kayitlar.txt"
+            );
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        List<string> cells = new List<string>();
+
+                        foreach (DataGridViewCell cell in row.Cells)
+                        {
+                            cells.Add(cell.Value?.ToString() ?? "");
+                        }
+
+                        // separate columns with |
+                        sw.WriteLine(string.Join("|", cells));
+                    }
+                }
+            }
+        }
+
+        private void LoadFromFile()
+        {
+            string filePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                "243154004 Arda Ceylan 05.05.2026",
+                "kayitlar.txt"
+            );
+
+            if (!File.Exists(filePath))
+                return;
+
+            dataGridView1.Rows.Clear();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] cells = line.Split('|');
+                dataGridView1.Rows.Add(cells);
+            }
+        }
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            txtfiyat.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+            string phone = txtfiyat.Text;
+            if (string.IsNullOrWhiteSpace(txturun.Text) ||
+            string.IsNullOrWhiteSpace(txtkategori.Text) ||
+            string.IsNullOrWhiteSpace(txtfiyat.Text))
+            {
+                MessageBox.Show("Lütfen zorunlu alanları doldurun!");
+                return;
+            }
+            
+            dataGridView1.Rows.Add(
+            txturun.Text,
+            txtkategori.Text,
+            txtfiyat.Text
+            );
+            lbldurum.Visible = true;
+            lbldurum.Text = "Kayıt Eklendi";
+            SaveToFile();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            txtfiyat.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+            string phone = txtfiyat.Text;
+            if (string.IsNullOrWhiteSpace(txturun.Text) ||
+            string.IsNullOrWhiteSpace(txtkategori.Text) ||
+            string.IsNullOrWhiteSpace(txtfiyat.Text))
+            {
+                MessageBox.Show("Lütfen zorunlu alanları doldurun!");
+                return;
+            }
+
+            if (dataGridView1.CurrentRow != null)
+            {
+                dataGridView1.CurrentRow.Cells[0].Value = txturun.Text;
+                dataGridView1.CurrentRow.Cells[1].Value = txtkategori.Text;
+                dataGridView1.CurrentRow.Cells[2].Value = txtfiyat.Text;
+                lbldurum.Visible = true;
+                lbldurum.Text = "Kayıt Güncellendi";
+                SaveToFile();
+            }
+        }
+
+
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+                lbldurum.Visible = true;
+                lbldurum.Text = "Kayıt Silindi";
+                SaveToFile();
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txturun.Clear();
+            txtkategori.Clear();
+            txtfiyat.Clear();
+            lbldurum.Visible = true;
+            lbldurum.Text = "Alanlar Temizlendi";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                txturun.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtkategori.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtfiyat.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 6;
+
+            dataGridView1.Columns[0].Name = "Ürün";
+            dataGridView1.Columns[1].Name = "Kategori";
+            dataGridView1.Columns[2].Name = "Fiyat";
+            LoadFromFile();
+
+        }
+
+        private void txturun_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtkategori_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                txturun.Text = row.Cells[0].Value?.ToString();
+                txtkategori.Text = row.Cells[1].Value?.ToString();
+                txtfiyat.Text = row.Cells[2].Value?.ToString();
+            }
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtfiyat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void formToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            foreach (Control c in this.Controls)
+            {
+                if (c != txtabout && c != menuStrip1)
+                    c.Visible = false;
+            }
+
+            txtabout.Visible = true;
+            txtabout.BringToFront();
+            txtabout.Text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        }
+
+        private void çıkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c != menuStrip1)
+                    c.Visible = true;
+            }
+
+            txtabout.Visible = false;
+        }
+    }   
+}
